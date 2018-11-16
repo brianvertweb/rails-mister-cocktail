83fbcee07b94ecc8c2ffcb36afe4d1383a05afd7
Brian Thomas <brian@vertwebsolutions.com>
2018-11-16 11:23:12 +0800

    re bundle installed the app

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,33 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to @cocktail
+  end
+
+  def show
+    @doses = @cocktail.doses
+  end
+end
+
+private
+
+def set_cocktail
+  @cocktail = Cocktail.find(params[:id])
+end
+
+def cocktail_params
+  # *Strong params*: You need to *whitelist* what can be updated by the user
+  # Never trust user data!
+  params.require(:cocktail).permit(:name)
+end
